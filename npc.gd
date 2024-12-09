9e8a252978f99f554e1bb337ec47d4c90ed1e110
JimmyAdrianDar <jimmyadriandar@gmail.com>
2024-12-09 23:21:41 +0800

    Npc (Temporary)

---
+++ npc.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@onready var interactive_area: InteractionArea = $InteractionArea 
+@onready var sprite = $Sprite2D
+
+func _ready() -> void:
+	interactive_area.interact = Callable(self, "_on_interact")
+
+func _on_interact():
+	Dialogic.start("npc")
